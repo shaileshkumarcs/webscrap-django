93f397c713291dacc82aaf253a90a499ad63d951
shailesh <shailesh.kumarcs@gmail.com>
2018-11-22 00:25:43 +0530

    Export in csv implemted

--- src/indeed/urls.py
+++ src/indeed/urls.py
@@ -6,4 +6,5 @@ app_name = 'web_scraping'
 
 urlpatterns = [
     url(r'^indeed/$', views.ScrapIndeed.as_view(), name='indeed'),
+    url(r'^exportcsv/$', views.ExportCsv.as_view(), name='exportcsv'),
 ]
--- src/indeed/views.py
+++ src/indeed/views.py
@@ -5,6 +5,8 @@ import tempfile
 from bs4 import BeautifulSoup
 import threading
 import time
+import csv
+import datetime
 
 import requests
 from django.contrib import messages
@@ -22,14 +24,49 @@ class ScrapIndeed(TemplateView):
         # browser = webdriver.PhantomJS()
         browser = webdriver.Chrome()
         # browser.get('https://stackoverflow.com/')
-        browser.get('https://www.indeed.co.in/jobs?q=php&l=Bengaluru%2C%20Karnataka&vjk=f1adbfff100cbb6d')
+        browser.get('https://www.indeed.co.in/jobs?q=php%20developer&l=Bengaluru%2C%20Karnataka&vjk=5986e28c994a6e6e')
         html = browser.page_source
         soup = BeautifulSoup(html, 'lxml')
-        # for link in soup.findAll('h2'):
-        # print(soup.find('div',{'id':'left-sidebar'}).findAll('a')[2].text)
+
+        name = 'ttt'#datetime.datetime.now().strftime("%d%m%y%H%I%p")
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename= "Details.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Job Title', 'Company Name', 'City', 'State','Job Details','Experience','Education'])
+
+        jobdetails = ''
         summ = soup.find('div', {'id': 'vjs-desc'})
         for su in summ.findAll('p'):
-            print(su.text)
-        # print(soup.find('div',{'id':'vjs-desc'}))
+            jobdetails += (su.text) #.encode('utf-8').strip()
 
-        return HttpResponse('Hello1')
+        writer.writerow(
+            [soup.find('div',{'id':'vjs-jobtitle'}).text,
+             soup.find('span',{'id':'vjs-cn'}).text,
+             soup.find('span',{'id':'vjs-loc'}).text,
+             soup.find('span',{'id':'vjs-loc'}).text,
+             jobdetails,
+             'Experience',
+             'Education'])
+        # print(soup.find('div',{'id':'vjs-jobtitle'}).text)
+        # print(soup.find('span',{'id':'vjs-cn'}).text)
+        # print(soup.find('span',{'id':'vjs-loc'}).text)
+        # summ = soup.find('div', {'id': 'vjs-desc'})
+        # for su in summ.findAll('p'):
+        #     print((su.text).encode('utf-8').strip())
+
+
+        return response
+
+
+class ExportCsv(TemplateView):
+    def get(self, request, *args, **kwargs):
+        # Create the HttpResponse object with the appropriate CSV header.
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+        writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+
+        return response
